9547e1ccac59521a0ba516df582e793c80ee62ad
Олег <proger@top-10.su>
2013-10-14 17:01:21 -0700

    Update mlp.php

--- LeNet_Example/mlp.php
+++ LeNet_Example/mlp.php
@@ -14,15 +14,15 @@
  */
 
 class HiddenLayer {
-	var $rng		= Array();	// Случайное число, используемое для инициализации весов
-	var $input;					// A symbolic tensor of shape (n_examples, n_in)
-	var $n_in		= 1;		// Рамзерность входа
-	var $n_out		= 1;		// Количество скрытых выходов
+	var $rng		= Array();		// Случайное число, используемое для инициализации весов
+	var $input;						// A symbolic tensor of shape (n_examples, n_in)
+	var $n_in		= 1;			// Рамзерность входа
+	var $n_out		= 1;			// Количество скрытых выходов
 	var $W			= false;
 	var $b			= false;
-	var $activation	= false;	// Нелинейность используемая в скрытом слое
+	var $activation	= "sigmoid";	// Нелинейность используемая в скрытом слое
 
-	function Init($input, $n_in, $n_out, $W=false, $b=false, $activation=false) {
+	function Init($input, $n_in, $n_out, $W=false, $b=false, $activation="sigmoid") {
 		/*
 		 * Обычно скрытые слои мнослойного персептрона - это блоки с сигмоидальной
 		 * функцией активации. Матрица весов W составляет форму ($n_in, $n_out)
@@ -33,21 +33,33 @@ class HiddenLayer {
 		 *
 		 * Скрытые блоки активации определяются по формуле: tanh(dot($input,$W) + $b)
 		 */
-		$this->input	= $input;
-		$this->n_in		= $n_in;
-		$this->n_out	= $n_out;
+		$this->input		= $input;
+		$this->n_in			= $n_in;
+		$this->n_out		= $n_out;
+		$this->W			= $W;
+		$this->b			= $b;
+		$this->activation	= $activation;
 
-		if(!$W) {
+		if(!$this->W) {
 			$low	= -sqrt(6./($this->n_in + $this->n_out));	// Минимальный вес
 			$hight	= sqrt(6./($this->n_in + $this->n_out));	// Максимальный вес
 
 			for($i=0; $i<$this->n_in; $i++) {
 				for ($j=0; $j<$this->n_out; $j++) {
-					$this->rng[$i][$j] = rand($low*100, $hight*100)/100;
+					$this->W[$i][$j] = rand($low*100, $hight*100)/100;
+
+					if($this->activation === "sigmoid") {
+						$this->W[$i][$j] *= 4;
+					}
 				}
 			}
+		}
 
-			//$W_values =
+		if(!$this->b) {
+			$this->b = Array();
+			for($i=0; $i<$this->n_out; $i++) {
+				$this->b[$i] = 0;
+			}
 		}
 	}
 }
@@ -56,6 +68,6 @@ class HiddenLayer {
 $obj = new HiddenLayer;
 $obj->Init(false, 16, 500);
 echo "<pre>";
-print_r($obj->rng);
+print_r($obj->W);
 echo "</pre>";
 ?>
